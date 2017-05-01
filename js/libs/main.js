88a340cc1e658be4d0f40487aac48a5b9613ed8c
Braulio Mateus <brawlyo@hotmail.com>
2017-05-01 17:47:43 -0300

    Upload
    upload

---
+++ js/libs/main.js
@@ -0,0 +1,8 @@
+$(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 1000,            // Integer: Speed of the transition, in milliseconds
+  timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
+  pager: false,           // Boolean: Show pager, true or false
+  nav: false,             // Boolean: Show navigation, true or false
+  
+});
